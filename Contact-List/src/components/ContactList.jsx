5ac9fba37f4478dcc5404fd891abd69f2e381e69
JonnelleC <carrjonnelle@gmail.com>
2024-07-20 16:51:13 -0400

    third commit

--- Contact-List/src/components/ContactList.jsx
+++ Contact-List/src/components/ContactList.jsx
@@ -1,34 +1,18 @@
-import React from "react"; 
-import ContactRow from "./ContactRow";
+import "./App.css";
+import { useState } from "react";
+import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
 
-const dummyContacts = [
-    { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
-    { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
-    { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
-  ];
+export default function App({ setSelectedContactId }) {
+    ContactList, setSelectedContactId = {setSelectedContactId} 
 
-export default function ContactList() { 
-const [contacts, setContacts] = useState(dummyContacts);
-console.log("Contacts: ", contacts)
-  return ( 
-        <table>
-          <thead>
-            <tr>
-              <th colSpan="3">Contact List</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Name</td>
-              <td>Email</td>
-              <td>Phone</td>
-            </tr>
-            {
-             contacts.map((contact) => {
-                return <ContactRow key={contact.id} contact={contact} />;
-              })
-             }
-          </tbody>
-        </table>
-    ); 
+  return (
+    <>
+      {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
+        <ContactList />
+      )}
+    </>
+  );
 }
